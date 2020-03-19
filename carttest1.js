b78095ffd1c2238f31b90d803727a1d40988fc62
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-03-19 09:15:19 -0400

    Add files via upload

---
+++ carttest1.js
@@ -0,0 +1,24 @@
+// I am leaving all the commented out items here because this is an example of how frigging hard I worked to get this crap TO WORK!!!!! We are talking "DAYS" here not hours!!!!!! and as of 4:40AM on Wednesday, March 18th, it is still not right!!!!
+
+//  var itemsOrdered = []
+ var item1 ={name:'Big Salad', cost: 10, ordered:0};
+ var item2= {name:'Small Salad', cost: 9, ordered:0};
+ var item3= {name: 'Hamburger Combo', cost:8, ordered:0};
+ var item4 ={name:'Cheeseburger Combo', cost:9, ordered:0};
+ var item5= {name:'Large Drink', cost:3, ordered:0};
+ var item6= {name:'Small Drink', cost:2, ordered:0};
+ var item7= {name:'Chocolate Milkshake', cost:4, ordered:0};
+
+//  var item1Total = document.addEventListener('click', addToBag());
+
+ function addToBag () {
+    document.addEventListener('click', addToBag);
+    // var inputElement = parseInt.inputElement;
+// return item1;
+    // var quantity = inputElement
+console.log(addToBag.target)
+    // if (isNaN(quantity)) {
+    //     console.log("Only numbers please...")
+    //     return;
+    //  }
+  }
