5737aa8af166df79b82c419083f01258f9eb0df2
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-03-22 04:32:18 -0400

    added text

--- tryingsomethingelse.js
+++ tryingsomethingelse.js
@@ -1,30 +1,34 @@
+
+// ****another thing to try John Smilga Youtube video*****
+
 // let item1 ={name:'Big Salad', cost: 10, ordered:0}; //<-object
+// variable.....
+const bagBtn = document.querySelector('.bag-btn');//yes
+const closeCartBtn = document.querySelector('.close-cart');//yes
+const clearCartBtn = document.querySelector('.clear-cart');//yes
+const clearOverlay = document.querySelector('.cart-overlay');//yes
+const cartItems = document.querySelector('.cart-items');//yes
+const cartTotal = document.querySelector('.cart-total');//yes
+const cartContent = document.querySelector('.cart-content');//yes
+const productsDOM = document.querySelector('.products-center');//yes
 
-// function item1Add () {
-//     // alert("Add to your bag?")//changes the text in the alert
-//     document.getElementById('item1Add').innerHTML= "The BIG Salad" + " $10"
-//    function onchange (item1) {
-    
-//    }
-//     onchange.getElementById('entry-total').innerHTML= i++
-// }
+// ^^^^i must have missed this part...this is in the nav bar section which I did not follow....but all items are in the html but different place....
 
-// ****another thing to try
-let item1 ={name:'Big Salad', cost: 10, ordered:0}; //<-object
 
-// const btn = document.querySelector('button');
-// const txt = document.querySelector('p');
 
-btn.addEventListener('click', update-prices);
 
-function onchange() {
-  if {
-      item1Add
-  }
-  } else {
-    btn.textContent = 'Start machine';
-    txt.textContent = 'The machine is stopped.';
-  }
-}
 
-// ****FUCK IT!!!!******
+
+
+
+
+// (function() {
+//   const cartInfo = document.getElementById('cart-info');  //div
+//   const cart = document.getElementById('cart');//??
+//   cartInfo.addEventListener('click', function() {
+//     cart.classList.toggle('show-cart')
+//   })
+  
+// });
+
+// ********
