3647b35714c3be82ea7ba75e769f3607146a4289
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-03-23 12:10:04 -0400

    added text

--- tryingsomethingelse.js
+++ tryingsomethingelse.js
@@ -2,18 +2,40 @@
 // ****another thing to try John Smilga Youtube video*****
 
 let item1 ={name:'Big Salad', cost: 10, ordered:0}; //<-object
-// variable.....
-const bagBtn = document.querySelector('.bag-btn');//yes
-const closeCartBtn = document.querySelector('.close-cart');//yes
-const clearCartBtn = document.querySelector('.clear-cart');//yes
-const clearOverlay = document.querySelector('.cart-overlay');//yes
-const cartItems = document.querySelector('.cart-items');//yes
-const cartTotal = document.querySelector('.cart-total');//yes
-const cartContent = document.querySelector('.cart-content');//yes
-const productsDOM = document.querySelector('.products-center');//yes
+let item2= {name:'Small Salad', cost: 9, ordered:0};//<-object
 
-//cart
-let cart = []
+function item1Add () {
+  alert("Add to your bag?")//changes the text in the alert
+  document.getElementById('item1Add').innerHTML= "The BIG Salad" + " $10"
+  document.addEventListener('click', multi)
+    function multi(i){
+      item1Add = i++
+    }
+  }
+
+// function item2Add () {
+//   alert("Add to your bag?")//changes the text in the alert
+//   document.getElementById('item2Add').innerHTML= "The Small Salad" + " $9"
+// }
+function removeAll() {
+  alert('Empty Your Bag?')
+  document.getElementById('h3').innerHTML= "";
+}
+
+
+
+// variable.....
+// const bagBtn = document.querySelector('.bag-btn');//yes
+// const closeCartBtn = document.querySelector('.close-cart');//yes
+// const clearCartBtn = document.querySelector('.clear-cart');//yes
+// const clearOverlay = document.querySelector('.cart-overlay');//yes
+// const cartItems = document.querySelector('.cart-items');//yes
+// const cartTotal = document.querySelector('.cart-total');//yes
+// const cartContent = document.querySelector('.cart-content');//yes
+// const productsDOM = document.querySelector('.products-center');//yes
+
+// //cart
+// let cart = []
 
 
 // ^^^^i must have missed this part...this is in the nav bar section which I did not follow....but all items are in the html but different place....
