c600f5ff5f2d32a491ec571b1851518de7f51e7e
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-03-25 15:19:49 -0400

    added text

---
+++ IfIEverGetThisToWork Items/onemorething.js
@@ -0,0 +1,19 @@
+
+
+
+const item1 ={name:'Big Salad', cost: 10, ordered:0}; //<-object
+var cartItems = []
+
+
+function item1Add () {
+    // alert("Add to your bag?")//changes the text in the alert
+    for (var i = 0; i < 20; i++){
+   item1[i] = cartItems
+    document.getElementById('item1Add').innerHTML= "The BIG Salad" + " $10"
+}
+}
+function btnOne () {
+    for (var i = 0; i < 20; i++) {
+        document.addEventListener ('click', item1).getElementById.innerHTML= "item1Add";
+    }
+}
